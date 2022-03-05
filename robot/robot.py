14a02af536fdde805a0b6566cfce64352879044b
Ricardo091203 <72629791+Ricardo091203@users.noreply.github.com>
2022-03-05 16:37:10 -0500

    Add climber

--- robot/robot.py
+++ robot/robot.py
@@ -1,29 +1,45 @@
 import wpilib
 import ctre
-import navx
 import magicbot
 from subsystems.drivetrain import DriveTrain
+from subsystems.climber import Climber
 
 
 class MyRobot(magicbot.MagicRobot):
     drivetrain: DriveTrain
+    climber: Climber
 
     def createObjects(self):
         self.joystick = wpilib.Joystick(0)
+
+        # drivetrain
         self.drive_l1 = ctre.WPI_VictorSPX(1)  #
         self.drive_l2 = ctre.WPI_VictorSPX(2)  #
         self.drive_r1 = ctre.WPI_VictorSPX(3)  #
         self.drive_r2 = ctre.WPI_VictorSPX(4)
         self.encoder_l = wpilib.Encoder(0, 1)
         self.encoder_r = wpilib.Encoder(2, 3)
-        self.nav = navx.AHRS.create_spi()
+        # self.nav = navx.AHRS.create_spi()
+
+        # climber
+        self.climbSol = wpilib.DoubleSolenoid(wpilib.PneumaticsModuleType.CTREPCM, 6, 7)
+        self.compressor = wpilib.Compressor(wpilib.PneumaticsModuleType.CTREPCM)
 
     def teleopInit(self):
         """Called when teleop starts; optional"""
 
     def teleopPeriodic(self):
         """Called on each iteration of the control loop"""
+
+        # drivetrain logic goes first
         self.drivetrain.move(self.joystick.getY(), -self.joystick.getX())
+
+        # climber control
+        if self.joystick.getRawButtonPressed(5):
+            self.climber.raise_hook()
+
+        if self.joystick.getRawButtonPressed(3):
+            self.climber.lower_hook()
 
 
 if __name__ == "__main__":
--- robot/subsystems/climber.py
+++ robot/subsystems/climber.py
@@ -1,2 +1,25 @@
+import wpilib
+import magicbot
+
+
 class Climber:
-    pass
+    climbSol: wpilib.DoubleSolenoid
+    compressor: wpilib.Compressor
+
+    state = magicbot.will_reset_to("off")
+
+    def raise_hook(self):
+        """Raise the climbing hook"""
+        self.state = "raise"
+
+    def lower_hook(self):
+        """Lower the climbing hook"""
+        self.state = "lower"
+
+    def execute(self):
+        if self.state == "raise":
+            self.climbSol.set(wpilib.DoubleSolenoid.Value.kForward)
+        elif self.state == "lower":
+            self.climbSol.set(wpilib.DoubleSolenoid.Value.kReverse)
+        else:
+            self.climbSol.set(wpilib.DoubleSolenoid.Value.kOff)
