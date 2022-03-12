aa1c56a5ac57ea2bb14b76a0a554b0b241907351
JonnyG511 <goodemilygirl3@gmail.com>
2022-03-12 14:39:08 -0500

    Add sparks, rename to electrical test

--- electrical-test/robot.py
+++ electrical-test/robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import wpilib
+import rev
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -8,6 +9,9 @@ class MyRobot(wpilib.TimedRobot):
         self.sol = wpilib.DoubleSolenoid(wpilib.PneumaticsModuleType.CTREPCM, 6, 7)
         self.joystick = wpilib.Joystick(0)
         self.compressor = wpilib.Compressor(wpilib.PneumaticsModuleType.CTREPCM)
+
+        self.intake_motor = rev.CANSparkMax(1, rev.CANSparkMax.MotorType.kBrushless)
+        self.shooter_motor = rev.CANSparkMax(2, rev.CANSparkMax.MotorType.kBrushless)
 
     def teleopPeriodic(self) -> None:
         b1 = self.joystick.getRawButton(5)
@@ -19,6 +23,14 @@ class MyRobot(wpilib.TimedRobot):
             self.sol.set(wpilib.DoubleSolenoid.Value.kReverse)
         else:
             self.sol.set(wpilib.DoubleSolenoid.Value.kOff)
+
+        self.intake_motor.set(self.joystick.getY())
+
+        b3 = self.joystick.getRawButton(11)
+        if b3:
+            self.shooter_motor.set(self.joystick.getZ())
+        else:
+            self.shooter_motor.set(0)
 
 
 if __name__ == "__main__":
