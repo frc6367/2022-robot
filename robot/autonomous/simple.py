c919836a0c7f5d252250d9109070e93da518aecf
Dustin Spicuzza <dustin@virtualroadside.com>
2022-03-25 19:41:44 -0400

    Add simple autonomous mode

---
+++ robot/autonomous/simple.py
@@ -0,0 +1,20 @@
+import magicbot
+from subsystems.shooter import Shooter
+from subsystems.drivetrain import DriveTrain
+
+
+class SimpleAutonomous(magicbot.AutonomousStateMachine):
+
+    drivetrain: DriveTrain
+    shooter: Shooter
+
+    MODE_NAME = "simple"
+    DEFAULT = True
+
+    @magicbot.timed_state(first=True, duration=3, next_state="backup")
+    def shoot(self):
+        self.shooter.shoot()
+
+    @magicbot.timed_state(duration=3)
+    def backup(self):
+        self.drivetrain.move(0.3, 0)
