3e3ed99f6e5057b634aba648b8032fddd0154c66
JonnyG511 <goodemilygirl3@gmail.com>
2022-04-10 13:27:00 -0400

    Add missing autonomous

---
+++ robot/autonomous/twoballturn.py
@@ -0,0 +1,60 @@
+import magicbot
+from components.pointer import Pointer
+
+from subsystems.intake import Intake
+from subsystems.shooter import Shooter
+from subsystems.drivetrain import DriveTrain
+
+
+class TwoBall(magicbot.AutonomousStateMachine):
+
+    drivetrain: DriveTrain
+    shooter: Shooter
+    intake: Intake
+    pointer: Pointer
+
+    MODE_NAME = "Two Ball - Turn"
+    DEFAULT = False
+
+    @magicbot.timed_state(first=True, duration=1, next_state="collect_ball")
+    def paused(self, initial_call):
+        if initial_call:
+            self.pointer.reset()
+
+    # go forward while intaking, 1 second
+    @magicbot.timed_state(duration=3, next_state="pause2")
+    def collect_ball(self):
+        self.drivetrain.move(-0.35, 0)
+        self.pointer.gotoAngle(0)
+        self.intake.activate()
+        # self.intake.force_intake_on()
+
+    @magicbot.timed_state(duration=0.5, next_state="turnaround")
+    def pause2(self):
+        self.drivetrain.move(0.1, 0)
+        self.intake.activate()
+        # self.intake.force_intake_on()
+
+    @magicbot.timed_state(duration=3, next_state="forward")
+    def turnaround(self):
+        # self.intake.force_intake_on()
+        self.pointer.gotoAngle(179.9)
+        # self.next_state("forward")
+
+    @magicbot.timed_state(duration=3.5, next_state="slightTurn")
+    def forward(self):
+        self.drivetrain.move(-0.3, 0)
+        self.pointer.gotoAngle(179.9)
+
+    @magicbot.timed_state(duration=1.5, next_state="shoot")
+    def slightTurn(self):
+        self.pointer.gotoAngle(155)
+
+    @magicbot.timed_state(duration=3.5, next_state="backup")
+    def shoot(self):
+        self.shooter.shoot()
+
+    @magicbot.timed_state(duration=3)
+    def backup(self):
+        self.shooter.shoot()
+        self.drivetrain.move(0.3, 0)
